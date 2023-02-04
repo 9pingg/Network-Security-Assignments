cdb69816fc14e16ad01ea24687122e09466fb5a1
Vedant <zcheeseyy@gmail.com>
2023-02-05 01:02:26 +0530

    match func

--- A1/a.py
+++ A1/a.py
@@ -1,24 +1,7 @@
 import random
 import itertools
-# TODO implement hash
+import hashlib
 
-
-# function to perform brute force to find key 
-def brute_force(ciphertext, plaintext):
-    p = ['AA', 'AB', 'AC', 'BB', 'BA', 'BC', 'CC', 'CA', 'CB']
-    e_p =  ['AA', 'AB', 'AC', 'BB', 'BA', 'BC', 'CC', 'CA', 'CB']
-    dics = []
-    for perm in itertools.permutations(p, len(e_p)):
-        dics.append(dict(zip(perm, e_p)))
-    print("total: ", len(dics))
-    keysChecked = 0
-    for d in dics:
-        if plaintext == decrypt(ciphertext, d): 
-            print("Found, total keys checked " , keysChecked, d)
-            break
-        keysChecked += 1
-    return "Not Found"    
-    
 # function to check whether the entered string is valid or not.
 def check(p_text, character_set):
     # only takes in strings with even length
@@ -36,7 +19,37 @@ def get_plain_text():
             return p_text
         else :
             continue
+# function to format the print statement
+def printt(msg, val):
+    p = "\n\n"
+    for i in range(100):
+        p+="#"
+    p+="\n"
+    print(msg + " " + str(val) + p)
 
+# returns random string of given length
+def generate_random_string(len):
+    character_set = "ABC"
+    s = ''.join(random.choices(character_set,k = len))
+    return s
+
+# function to hash a given string
+def hash(user_text, toPrint):
+    hash_256 = hashlib.sha256(user_text.encode('utf-8')).hexdigest()
+    hashstring = ""
+    
+    # MATCHING A = 0 - 5, B = 6 - 10, C = 11 - 16
+
+    for b in hash_256:
+        dec = int(b, 16)
+        if dec <= 5 : hashstring += 'A'
+        elif dec <= 10 : hashstring += 'B'
+        else : hashstring += 'C'
+    if toPrint: 
+        printt("hash: ", hashstring)
+    #printt("length: ", len(hashstring))
+    return hashstring
+        
 # function returns a random key
 def get_key():
     p = ['AA', 'AB', 'AC', 'BB', 'BA', 'BC', 'CC', 'CA', 'CB']
@@ -67,20 +80,40 @@ def decrypt(cipher_text, key):
 # to check if the hash of decrypted_user_text matches the hash.
 def checkMatch(decrypted_text):
     n = len(plain_text)
-    decrypted_user_text = decrypted_text[0:n//2]
-    decrypted_hash = decrypted_text[n//2:]  
-    if decrypted_hash == hash(decrypted_user_text): print("Its a match")
+    decrypted_user_text = decrypted_text[0:n-64]
+    decrypted_hash = decrypted_text[-64:]  
+    print("hashing the decrypted_user_text to check if it matches the decrypted_hash")
+    if decrypted_hash == hash(decrypted_user_text, 0): printt("Its a match, after hashing the user_text: ", hash(decrypted_user_text, 0))
     else : print("Wrong Hash")
 
+# function to perform brute force to find key 
+def brute_force(ciphertext, plaintext):
+    p = ['AA', 'AB', 'AC', 'BB', 'BA', 'BC', 'CC', 'CA', 'CB']
+    e_p =  ['AA', 'AB', 'AC', 'BB', 'BA', 'BC', 'CC', 'CA', 'CB']
+    dics = []
+    for perm in itertools.permutations(p, len(e_p)):
+        dics.append(dict(zip(perm, e_p)))
+    print("total possible keys: ", len(dics))
+    keysChecked = 0
+    for d in dics:
+        if plaintext == decrypt(ciphertext, d): 
+            print("Key Found: ", d)
+            printt("total keys checked: " , keysChecked)
+            break
+        keysChecked += 1
+    return "Not Found"    
+    
 if __name__ == "__main__":   
-    user_text = get_plain_text()
-    plain_text = user_text + hash(user_text)
-    print(plain_text)
+    # user_text = get_plain_text()
+    user_text = generate_random_string(100)
+    printt("user_text:", user_text)
+    plain_text = user_text + hash(user_text,1)
+    printt("plain_text:", plain_text)
     key = get_key()
-    print(key)
+    printt("key:", key)
     cipher_text = encrypt(plain_text, key)
-    print(cipher_text)
+    printt("cipher_text:", cipher_text)
     decrypted_text = decrypt(cipher_text, key)
-    print(decrypted_text)
+    printt("decrypted_text:", decrypted_text)
     checkMatch(decrypted_text)
     brute_force(cipher_text, plain_text)
