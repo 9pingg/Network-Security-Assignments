c855b5d3ec75043ea8916a86c3c1bc90dd23951e
Vedant <zcheeseyy@gmail.com>
2023-04-20 18:08:28 +0530

    A4

--- A4/client.py
+++ A4/client.py
@@ -4,38 +4,71 @@ import ssl
 import datetime
 from base64 import b64decode
 from base64 import b64encode
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Hash import SHA256
+import os
+
 # Load the SSL/TLS certificate of the TSA server
-tls_cert = "/Users/vedan/Desktop/NS-Assignments/A4/cert.pem"
-client_cert = "/Users/vedan/Desktop/NS-Assignments/A4/cert_c.pem"
-client_key = "/Users/vedan/Desktop/NS-Assignments/A4/key_c.pem"
+tsa_cert = "/Users/vedan/Desktop/NS-Assignments/A4/ssl/cert_s.pem"
+client_cert = "/Users/vedan/Desktop/NS-Assignments/A4/ssl/cert_c.pem"
+client_key = "/Users/vedan/Desktop/NS-Assignments/A4/ssl/key_c.pem"
 # Connect to the TSA server over an encrypted connection
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.load_cert_chain(certfile=client_cert, keyfile=client_key)
-context.load_verify_locations(cafile=tls_cert)
-
-
-
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+ssl_context.load_cert_chain(certfile=client_cert, keyfile=client_key)
+ssl_context.load_verify_locations(cafile=tsa_cert)
 
 
 # Generate a hash of the file to be timestamped
-filename = "cert_generate.txt"
-with open(filename, "rb") as f:
+fp = "cert_generate.txt"
+with open(fp, "rb") as f:
     file_data = f.read()
     file_hash = hashlib.sha256(file_data).hexdigest()
 
+hash_func = SHA256.new()
 server_address = ('localhost', 10000)
 with socket.create_connection(server_address) as sock:
-    with context.wrap_socket(sock, server_hostname=server_address[0]) as ssock:
+    with ssl_context.wrap_socket(sock, server_hostname=server_address[0]) as ssock:
         # Send the file hash to the TSA server
         ssock.sendall(file_hash.encode())
 
         # Receive the signed timestamp from the TSA server
-        timestamp_bytes = ssock.recv(1024)
-        hash, signature, timestamp = timestamp_bytes.decode().split(",")
-        hash = b64decode(hash)
+        timestamp_bytes = ssock.recv(4096)
+        
+        signature, timestamp, tsa_pub_key = timestamp_bytes.decode().split(",")
         signature = b64decode(signature)
         timestamp = b64decode(timestamp)
-        print(timestamp)
-        print(signature, timestamp)
+        tsa_pub_key = b64decode(tsa_pub_key)
 
-# Verify the signed timestamp using the TSA's public key (not implemented in this example)
+        
+        tsa_pub_key_n = RSA.import_key(tsa_pub_key)
+        cipher = PKCS1_OAEP.new(tsa_pub_key_n)
+        decrypted_signature = cipher.decrypt(signature)
+        
+        
+        data_with_tstamp_pubkey = file_hash.encode() + timestamp + tsa_pub_key        
+        hash_func.update(data_with_tstamp_pubkey)
+        timestamped_hash = hash_func.digest()
+        
+        print(decrypted_signature)
+        print(timestamped_hash)
+
+        if decrypted_signature == timestamped_hash: 
+            print("verified")
+            print("File Hash: ", file_hash.encode())
+            print("Timestamp: ", timestamp)
+            print("Public Key TSA: ", b64encode(tsa_pub_key))
+            print("Signature: ", signature)
+
+            fp = "client_a_cert_tsa.txt"
+            with open(fp, 'wb') as f:
+                f.write(file_hash.encode())
+                f.write(os.linesep.encode())
+                f.write(timestamp)
+                f.write(os.linesep.encode())
+                f.write(b64encode(tsa_pub_key))
+                f.write(os.linesep.encode())
+                f.write(b64encode(signature))
+
+        
+
--- A4/server.py
+++ A4/server.py
@@ -4,73 +4,67 @@ import datetime
 import ssl
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Signature import pkcs1_15
+
 from base64 import b64decode
 from base64 import b64encode
 
 # Path to the TLS certificate and private key
-tls_cert = "/Users/vedan/Desktop/NS-Assignments/A4/cert.pem"
-tls_key = "/Users/vedan/Desktop/NS-Assignments/A4/key.pem"
+tsa_cert = "/Users/vedan/Desktop/NS-Assignments/A4/ssl/cert_s.pem"
+tsa_key = "/Users/vedan/Desktop/NS-Assignments/A4/ssl/key_s.pem"
 
 
 # Create an SSL context
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile=tls_cert, keyfile=tls_key)
-context.load_verify_locations(cafile=tls_cert)
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(certfile=tsa_cert, keyfile=tsa_key)
+ssl_context.load_verify_locations(cafile=tsa_cert)
 
 # Initialize the RSA key and hash function
 rsa_key = RSA.generate(2048)
-hash_func = SHA256.new()
+private_key = rsa_key.publickey().export_key()
+tsa_pub_key = rsa_key.export_key()
 
-# Define the TSA server
+s = PKCS1_OAEP.new(rsa_key)
+
+
 def tsa_server():
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Bind the socket to a specific address and port
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ('localhost', 10000)
     print(f"Starting TSA server on {server_address[0]}:{server_address[1]}")
     sock.bind(server_address)
-
-    # Listen for incoming connections
     sock.listen(1)
 
-    with open("server.pub", "rb") as f:
-        tsa_pub_key = ssl.DER_cert_to_PEM_cert(f.read())
-    # Start the main loop
     while True:
         print("Waiting for a connection...")
-        # Wait for a connection
-        connection, client_address = sock.accept()
-
+        c, client_ip = sock.accept()
         # Wrap the socket with SSL/TLS
-        ssl_conn = context.wrap_socket(connection, server_side=True)
+        sls_connection = ssl_context.wrap_socket(c, server_side=True)
 
         try:
-            print(f"Connection from {client_address}")
-
+            print(f"Connection from {client_ip}")
             # Receive the hash from the client
-            data = ssl_conn.recv(1024)
+            data = sls_connection.recv(4096)
 
             if data:
                 # Calculate the timestamp
                 timestamp = datetime.datetime.now().isoformat().encode('utf-8')
-                print(timestamp)
-                print(tsa_pub_key)
+
                 # Calculate the hash of the timestamped hash
                 data_with_tstamp_pubkey = data + timestamp + tsa_pub_key
-                # hash_func.update(data)
+                hash_func = SHA256.new()
                 hash_func.update(data_with_tstamp_pubkey)
                 timestamped_hash = hash_func.digest()
 
                 # Sign the timestamped hash with the TSA's private key
-                signature = pkcs1_15.new(rsa_key).sign(hash_func)
+                signature = s.encrypt(timestamped_hash)
 
-                # Construct the response message
-                response = b64encode(timestamped_hash).decode('utf-8') + "," + b64encode(signature).decode('utf-8') + "," + b64encode(timestamp).decode('utf-8') + b64encode(tsa_pub_key).decode('utf-8')
-                print(response)
-                # Send the response to the client
-                ssl_conn.sendall(response.encode('utf-8'))
+                # response from server
+                response = b64encode(signature).decode('utf-8') + "," + b64encode(timestamp).decode('utf-8') + "," + b64encode(tsa_pub_key).decode('utf-8')
+
+                print("certificate send to client:\n", response)
+                sls_connection.sendall(response.encode('utf-8'))
                 
             else:
                 print("No data received from client")
@@ -80,8 +74,8 @@ def tsa_server():
 
         finally:
             # Close the connection
-            ssl_conn.shutdown(socket.SHUT_RDWR)
-            ssl_conn.close()
+            sls_connection.shutdown(socket.SHUT_RDWR)
+            sls_connection.close()
 
 if __name__ == "__main__":
     tsa_server()
